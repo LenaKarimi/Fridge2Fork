7771c41b040d029d5e40e20df0ac40e2cb3157e1
intisaar02a-pixel <intisaar02a@gmail.com>
2026-04-05 14:10:09 +0200

    Update Profile.java
    Getters/setters för profil + kosntruktor

--- src/main/java/Model/Profile.java
+++ src/main/java/Model/Profile.java
@@ -4,4 +4,25 @@ public class Profile {
 
     private String name;
     private int id;
+
+    public Profile(String name, int id){
+        this.name = name;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
