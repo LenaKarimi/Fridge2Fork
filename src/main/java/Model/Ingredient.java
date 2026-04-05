bf0ccc472cdaf7e265a9762e14b85cf18df1ef5b
intisaar02a-pixel <intisaar02a@gmail.com>
2026-04-05 14:06:41 +0200

    Create Ingredient.java
    Ingrediens klassen skapad

---
+++ src/main/java/Model/Ingredient.java
@@ -0,0 +1,15 @@
+package Model;
+
+public class Ingredient {
+
+    private String name;
+    private Category category;
+
+    public Ingredient(String name, Category category){
+
+        this.name = name;
+        this.category = category;
+    }
+
+
+}
