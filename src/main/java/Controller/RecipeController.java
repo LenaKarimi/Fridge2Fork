9562a2252f07869a78beb3cea8c65cc0612728ab
intisaar02a-pixel <intisaar02a@gmail.com>
2026-04-20 00:11:10 +0200

    Update RecipeController.java

--- src/main/java/Controller/RecipeController.java
+++ src/main/java/Controller/RecipeController.java
@@ -13,6 +13,7 @@ import java.util.List;
 public class RecipeController {
     //hanterar logik kring recept, 70% gräns osv
 
+    //De olika klasserna som används från Api:et
     private MealRepository mealRepository;
     private MealMapper mealMapper;
     private HttpTheMealDbClient httpTheMealDbClient;
@@ -23,8 +24,13 @@ public class RecipeController {
         this.mealMapper = new MealMapper();
 
     }
-
+     //Funktion för att ska en arraylist av de ingredienser som finns baserat på det primära ingrediensen
     public List<Recipe> searchRecipes (String mainIngredient) throws Exception {
+
+        if(mainIngredient==null || mainIngredient.isBlank()){
+            return new ArrayList<>();
+        }
+
         List<Recipe> recipes = new ArrayList<>();
 
         List<TheMealDbDTO> meals = mealRepository.getMealsByIngredient(mainIngredient);
@@ -40,5 +46,19 @@ public class RecipeController {
         }
         return recipes;
 
+    }
+
+    //Gör om objekt till strängar detta ska användas i gui sen.
+    public List<String> searchRecipeNames(String mainIngredient) throws Exception{
+        List<Recipe> recipes = searchRecipes(mainIngredient);
+
+        List<String> names = new ArrayList<>();
+
+        for (Recipe recipe : recipes){
+            names.add(recipe.getName());
+
+
+        }
+        return names;
     }
 }
