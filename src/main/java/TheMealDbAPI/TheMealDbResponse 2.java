87266cb2f430fb6e2840ae31199d3602c7f8a166
LenaKarimi <lena.karimi@hotmail.com>
2026-04-19 14:40:57 +0200

    Tar bort response dublett

--- src/main/java/TheMealDbAPI/TheMealDbResponse 2.java
+++
@@ -1,8 +0,0 @@
-package API;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import java.util.List;
-
-@JsonIgnoreProperties (ignoreUnknown = true)
-public record TheMealDbResponse (List<TheMealDbDTO> meals){}
-
-//denna klass tar bara emot meals
