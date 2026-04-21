1c7bb228e577d0151a688425ff68b462da1019be
intisaar02a-pixel <intisaar02a@gmail.com>
2026-04-21 13:46:26 +0200

    Update ControllerTest.java

--- src/main/java/Controller/ControllerTest.java
+++ src/main/java/Controller/ControllerTest.java
@@ -8,26 +8,26 @@ import java.util.List;
 
 public class ControllerTest {
     public static void main (String[] args){
-        try{
-            RecipeController recipeController = new RecipeController();
-            List<Recipe> chickenrecipes = recipeController.searchRecipes("chicken");
-            List<Recipe> beefrecipes = recipeController.searchRecipes("beef");
-            List<String> names = recipeController.searchRecipeNames("chicken");
+       // try{
+           // RecipeController recipeController = new RecipeController();
+           // List<Recipe> chickenrecipes = recipeController.searchRecipes("chicken");
+           // List<Recipe> beefrecipes = recipeController.searchRecipes("beef");
 
-            for (String name : names){
-                System.out.println (name);
-            }
 
-            for (Recipe recipe : chickenrecipes){
-               System.out.println(recipe.getName());
-            }
+           // for (Name name : names){
+             //   System.out.println (name);
+           // }
 
-            for (Recipe recipe : beefrecipes){
-                System.out.println(recipe.getName());
-            }
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+           // for (Recipe recipe : chickenrecipes){
+             //  System.out.println(recipe.getName());
+           // }
+
+           // for (Recipe recipe : beefrecipes){
+          //      System.out.println(recipe.getName());
+          //  }
+       // } catch (Exception e) {
+        //    throw new RuntimeException(e);
+       // }
     }
 
 
