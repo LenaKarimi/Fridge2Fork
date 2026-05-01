b97ddcf050e754389827ef7dca47be3a001368df
intisaar02a-pixel <intisaar02a@gmail.com>
2026-05-01 16:56:10 +0200

    Update RecipeController.java
    Städat upp koden och tagit bort gamla metoder som inte behövs

--- src/main/java/Controller/RecipeController.java
+++ src/main/java/Controller/RecipeController.java
@@ -40,7 +40,7 @@ public class RecipeController {
         validateAllCategories(categoryMap);
 
         //samla alla valda ingredienser och rensa bort none valen inna vi skickar vidar
-        List <String> userFridge = extractAllingredients(categoryMap);
+        List <String> userFridge = extractAllIngredients(categoryMap);
 
         //Sökningen sker här för möjliga recept baserad på valda ingredienser
         Set<String> discoveredMealIds = new HashSet<>();
@@ -48,11 +48,11 @@ public class RecipeController {
 
         for(String ingredient : userFridge){
             //här sker förfrågningen om specifik ingrediens
-            List<TheMealDbDTO> apiResponse = mealRepisotory.getMealsByIngredient(ingredient);
+            List<TheMealDbDTO> apiResponse = mealRepository.getMealsByIngredient(ingredient);
              //om inte ingrediens finns ska programmet fortsätta till nästa ignrediens och inte krascha
-            if (apiRespone == null) continue;
+            if (apiResponse == null) continue;
 
-            for (TheMealDbDTO mealSummary : apiRespone){
+            for (TheMealDbDTO mealSummary : apiResponse){
                 //sparar inte recept som redan är hämtade.
                 if (discoveredMealIds.contains(mealSummary.idMeal)) continue;
                 discoveredMealIds.add(mealSummary.idMeal);
@@ -68,7 +68,7 @@ public class RecipeController {
                 if (calculateMatchPercentage(recipeObject, userFridge) >= 0.5){
                     matchingRecipes.add(recipeObject);
                 }
-                if (matchingRecipes.size() >= 4 return matchingRecipes);
+                if (matchingRecipes.size() >= 4) return matchingRecipes;
 
 
             }
@@ -82,8 +82,8 @@ public class RecipeController {
             List<String> selections = categoryMap.get(categoryName);
 
             //om en sak inte valts per alla kategorier skickas felmeddelande.
-            if (selections == null || selection.isEmpty()){
-                throw new IllegalArgumentException("Du saknar val i kategorin : " + categoryname);
+            if (selections == null || selections.isEmpty()){
+                throw new IllegalArgumentException("Du saknar val i kategorin : " + categoryName);
             }
         }
     }
@@ -91,7 +91,7 @@ public class RecipeController {
     //här skapas lista med valda ingredienser och none exkluderas.
     private List<String> extractAllIngredients(Map<String, List<String>> categoryMap){
         List <String> allIngredients = new ArrayList<>();
-        for ( List<String> ingredientList : categorMap.valus()){
+        for ( List<String> ingredientList : categoryMap.values()){
             for (String ingredient : ingredientList){
                 if (!ingredient.equalsIgnoreCase("none")){
 
@@ -108,12 +108,12 @@ public class RecipeController {
     private boolean isCorrectCuisine(Recipe recipe, List<Cuisine> selectedCuisines){
         //om användaren ej val kök godkänns alla recept
         //ska man ha ett val för none kanske??
-        if (selectedCuisines == null || selectedCusines.isEmpty()) return true;
+        if (selectedCuisines == null || selectedCuisines.isEmpty()) return true;
         return selectedCuisines.contains(recipe.getCuisine());
     }
 
     //här sker beräkningen av receptets ingredienser som matchar anvädnarens val
-    private double calculateMatchPercentage(Recipe recipe, List<String> userFride){
+    private double calculateMatchPercentage(Recipe recipe, List<String> userFridge){
         double matchCount = 0;
         List<Ingredient> recipeIngredients = recipe.getIngredients();
 
@@ -127,69 +127,10 @@ public class RecipeController {
         return matchCount / recipeIngredients.size();
     }
 
-    //Funktion för att ska en arraylist av de ingredienser som finns baserat på det primära ingrediensen
-    public List<Recipe> searchRecipes(String mainIngredient) throws Exception {
-
-        if (mainIngredient == null || mainIngredient.isBlank()) {
-            return new ArrayList<>();
-        }
-
-        List<Recipe> recipes = new ArrayList<>();
-
-        List<TheMealDbDTO> meals = mealRepository.getMealsByIngredient(mainIngredient);
-
-        for (TheMealDbDTO meal : meals) {
-            TheMealDbDTO detailedMeal = mealRepository.getMealById(meal.idMeal);
-
-            Recipe recipe = mealMapper.toDomain(detailedMeal);
 
 
-            recipes.add(recipe);
-        }
-        return recipes;
-    }
-
-    public List<Recipe> searchRecipesByIngredients(List<String> ingredients) throws Exception {
-        if (ingredients == null || ingredients.isEmpty()) {
-            throw new IllegalArgumentException("You have to choose at leats one alternative in all categories to continue!");
-        }
-
-        List<Recipe> allRecipes = new ArrayList<>();
-
-        for (String ingredient : ingredients) {
-
-            if (ingredient == null || ingredient.isBlank() || ingredient.equalsIgnoreCase("none")) {
-                continue;
-            }
 
 
-            List<TheMealDbDTO> meals = mealRepository.getMealsByIngredient(ingredient);
-
-            for (TheMealDbDTO meal : meals) {
-                TheMealDbDTO detailedMeal = mealRepository.getMealById(meal.idMeal);
-                Recipe recipe = mealMapper.toDomain(detailedMeal);
-                allRecipes.add(recipe);
-
-
-            }
-
-        }
-        return allRecipes;
-    }
-
-    //Gör om objekt till strängar detta ska användas i gui sen.
-    public List<String> searchRecipeNamesByIngredients(List<String> ingredients) throws Exception {
-        List<Recipe> recipes = searchRecipesByIngredients(ingredients);
-
-        List<String> names = new ArrayList<>();
-
-        for (Recipe recipe : recipes) {
-            names.add(recipe.getName());
-
-
-        }
-        return names;
-    }
 
     //recipe
     //gör om om objektet från ett modelobjekt till ett DTO-objekt
@@ -206,24 +147,7 @@ public class RecipeController {
         return dtos;
     }
 
-    public List<RecipeDTO> getFilteredRecipes (List<String> selectedIngredients) throws Exception{
-        //Om listan är tom som visas i guit
-        if (selectedIngredients == null || selectedIngredients.isEmpty()){
-            throw new IllegalArgumentException("Vänligen välj minst en ingrediens innan du går vidare!");
-        }
 
-        //hämta domänobjekt
-        List<Recipe> allFoundRecipes = searchRecipesByIngredients(selectedIngredients);
-
-        //kontrollera om api:et hittade något
-        if (allFoundRecipes == null || allFoundRecipes.isEmpty()){
-            return new ArrayList<>();
-        }
-
-        //konvertera domänobjekt till dtoer som guit kan hanter
-        return getRecipeDTOList(allFoundRecipes);
-
-    }
 
 
 
