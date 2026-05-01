c611f0fa19338ba33bb7b2999fc179fb92d40ca0
intisaar02a-pixel <intisaar02a@gmail.com>
2026-05-01 16:44:35 +0200

    Update RecipeController.java
    lagt till nya metoder som ska användas från och med nu

--- src/main/java/Controller/RecipeController.java
+++ src/main/java/Controller/RecipeController.java
@@ -9,9 +9,7 @@ import TheMealDbAPI.HttpTheMealDbClient;
 import DTO.*;
 import TheMealDbAPI.TheMealDbDTO;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class RecipeController {
     //hanterar logik kring recept, 70% gräns osv
@@ -35,11 +33,98 @@ public class RecipeController {
      */
 
     //Funktion för att ska en arraylist av de ingredienser som finns baserat på det primära ingrediensen
-    public List<Recipe> searchRecipes(Map<String>, List<String> categoryMap, List <Cuisine> selectedCuisines) throws Exception {
+    public List<Recipe> searchRecipes(Map<String, List<String>> categoryMap, List <Cuisine> selectedCuisines) throws Exception {
 
-        //Kontrollera att varje 
+        //Kontrollera att varje kategor i Gui har fått minst ett val none inkluderad.
+
+        validateAllCategories(categoryMap);
+
+        //samla alla valda ingredienser och rensa bort none valen inna vi skickar vidar
+        List <String> userFridge = extractAllingredients(categoryMap);
+
+        //Sökningen sker här för möjliga recept baserad på valda ingredienser
+        Set<String> discoveredMealIds = new HashSet<>();
+        List<Recipe> matchingRecipes = new ArrayList<>();
+
+        for(String ingredient : userFridge){
+            //här sker förfrågningen om specifik ingrediens
+            List<TheMealDbDTO> apiResponse = mealRepisotory.getMealsByIngredient(ingredient);
+             //om inte ingrediens finns ska programmet fortsätta till nästa ignrediens och inte krascha
+            if (apiRespone == null) continue;
+
+            for (TheMealDbDTO mealSummary : apiRespone){
+                //sparar inte recept som redan är hämtade.
+                if (discoveredMealIds.contains(mealSummary.idMeal)) continue;
+                discoveredMealIds.add(mealSummary.idMeal);
+
+                //hämta fullständiga detaljer dvs ingredienser, isntruktioner osv.
+                TheMealDbDTO detailedMeal = mealRepository.getMealById(mealSummary.idMeal);
+                Recipe recipeObject = mealMapper.toDomain(detailedMeal);
+
+                //filterring baserad på kök
+                if (!isCorrectCuisine(recipeObject, selectedCuisines)) continue;
+
+                //beräkna 50% matchningen
+                if (calculateMatchPercentage(recipeObject, userFridge) >= 0.5){
+                    matchingRecipes.add(recipeObject);
+                }
+                if (matchingRecipes.size() >= 4 return matchingRecipes);
 
 
+            }
+        }
+        return matchingRecipes;
+    }
+
+    //här kontrolleras att minst ett val gjorts per kategor.
+    private void validateAllCategories(Map<String, List<String>> categoryMap){
+        for (String categoryName : categoryMap.keySet()){
+            List<String> selections = categoryMap.get(categoryName);
+
+            //om en sak inte valts per alla kategorier skickas felmeddelande.
+            if (selections == null || selection.isEmpty()){
+                throw new IllegalArgumentException("Du saknar val i kategorin : " + categoryname);
+            }
+        }
+    }
+
+    //här skapas lista med valda ingredienser och none exkluderas.
+    private List<String> extractAllIngredients(Map<String, List<String>> categoryMap){
+        List <String> allIngredients = new ArrayList<>();
+        for ( List<String> ingredientList : categorMap.valus()){
+            for (String ingredient : ingredientList){
+                if (!ingredient.equalsIgnoreCase("none")){
+
+                    //HÄR görs ingredienser om till små bokstäver för att matcha api
+                    //kolla senare om detta med understreck
+                    allIngredients.add(ingredient.toLowerCase());
+                }
+            }
+        }
+        return allIngredients;
+    }
+
+    //kontroll om receptets ursprung matchar användarens val av kök
+    private boolean isCorrectCuisine(Recipe recipe, List<Cuisine> selectedCuisines){
+        //om användaren ej val kök godkänns alla recept
+        //ska man ha ett val för none kanske??
+        if (selectedCuisines == null || selectedCusines.isEmpty()) return true;
+        return selectedCuisines.contains(recipe.getCuisine());
+    }
+
+    //här sker beräkningen av receptets ingredienser som matchar anvädnarens val
+    private double calculateMatchPercentage(Recipe recipe, List<String> userFride){
+        double matchCount = 0;
+        List<Ingredient> recipeIngredients = recipe.getIngredients();
+
+        for (Ingredient ing : recipeIngredients){
+            String name = ing.getName().toLowerCase();
+
+            if (userFridge.contains(name)){
+                matchCount++;
+            }
+        }
+        return matchCount / recipeIngredients.size();
     }
 
     //Funktion för att ska en arraylist av de ingredienser som finns baserat på det primära ingrediensen
