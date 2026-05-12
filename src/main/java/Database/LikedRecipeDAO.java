eab941978c69e7a9debc11d184881cacddf06b7e
noni <racilzanganeh@gmail.com>
2026-05-13 00:43:54 +0200

    massa ändringar i främst view
    det som gjort är; korrigeringar i DAO så att databas kopplingen blir korrekt, tom konstruktor i ingridient model klass så att json omformateringen blir korrekt, i recipeview har debugger adderats rad 64, vad som visas om man har likade recet har ändrats och små grejer för att försöka lösa formateringen som rad 116 och 93. I recipeResults har kod laggts till för att hjärtat inte bara ska vara en knapp utan logiken bakom rellaterat till db rad 147-173. I side bar view har det ändrats så att om användare är inloggad visa recept/du har ej sparat recept. om current user == null derrigera om till logg in view.  rad 70-80

--- src/main/java/Database/LikedRecipeDAO.java
+++ src/main/java/Database/LikedRecipeDAO.java
@@ -8,7 +8,7 @@ public class LikedRecipeDAO {
     // sparar likat recept till en användare
     public void likedRecipe(int profieId, String mealId) throws SQLException {
         //lägger in båda idn i tabellen, dubletter ignoreras
-        String sql = "INSERT INTO liked_recipes (profie_id, meal_id) VALUES (?, ?) ON CONFLICT DO NOTHING";
+        String sql = "INSERT INTO liked_recipes (profile_id, meal_id) VALUES (?, ?) ON CONFLICT DO NOTHING";
         try (Connection connection = DbConnection.getConnection()) {
             PreparedStatement preparedStatement = connection.prepareStatement(sql);
             preparedStatement.setInt(1, profieId); // lägger in frågetecken
@@ -20,7 +20,7 @@ public class LikedRecipeDAO {
     //avlika recept
     public void unlikeRecipe(int profieId, String mealId) throws SQLException {
         //tar bort rad där båda idn matchar
-        String sql = "DELETE FROM liked_recipes WHERE profie_id = ? AND meal_id = ?";
+        String sql = "DELETE FROM liked_recipes WHERE profile_id = ? AND meal_id = ?";
         try (Connection connection = DbConnection.getConnection()) {
             PreparedStatement preparedStatement = connection.prepareStatement(sql);
             preparedStatement.setInt(1, profieId);
@@ -32,7 +32,7 @@ public class LikedRecipeDAO {
     // vet ej om nödvändigt men kontrollerar om ett recept är gillat
     public boolean isLiked(int profieId, String mealId) throws SQLException {
         // söker efter en rad i tabbelen där idn matchar
-        String sql = "SELECT 1 * FROM liked_recipes WHERE profie_id = ? AND meal_id = ?";
+        String sql = "SELECT 1 FROM liked_recipes WHERE profile_id = ? AND meal_id = ?";
         try (Connection connection = DbConnection.getConnection()) {
             PreparedStatement preparedStatement = connection.prepareStatement(sql);
             preparedStatement.setInt(1, profieId);
