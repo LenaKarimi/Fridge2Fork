21c1911310aee57fa603f191b60c6831bd95cb53
noni <racilzanganeh@gmail.com>
2026-05-24 18:45:24 +0200

    Update PantryController.java
    en add metod i controllern

--- src/main/java/Controller/PantryController.java
+++ src/main/java/Controller/PantryController.java
@@ -9,4 +9,33 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PantryController {
+
+    private final PantryItemDAO pantryItemDAO = new PantryItemDAO();
+
+
+    // lägger till produkt, returenerar null om lyckat, felmedelande om problem
+    public String addItem(int profileId, String name, String expiryDate) throws SQLException {
+        if(name == null || name.isBlank()) {
+            return "Write a product name";
+        }
+
+        if(expiryDate == null || expiryDate.isBlank()) {
+            return "Write a expiry date";
+        }
+
+        try {
+            LocalDate date = LocalDate.parse(expiryDate);
+
+            if(date.isBefore(LocalDate.now())) {
+                return "Expiry date cannot be in the past";
+            }
+
+            pantryItemDAO.addItem(profileId, name, date);
+            return null;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "Error";
+        }
+    }
 }
