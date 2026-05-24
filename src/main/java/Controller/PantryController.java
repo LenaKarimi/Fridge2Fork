87ca2335c734fe8ff48a50f8b5f561dafae03821
noni <racilzanganeh@gmail.com>
2026-05-24 18:39:21 +0200

    skapar en controller till funktionen pantry item som view senare kan anroppa

---
+++ src/main/java/Controller/PantryController.java
@@ -0,0 +1,12 @@
+package Controller;
+
+import Database.*;
+import Model.*;
+
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PantryController {
+}
