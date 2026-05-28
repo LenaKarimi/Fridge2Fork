525f59b909f8dcb4280402827ecd60668906834e
intisaar02a-pixel <intisaar02a@gmail.com>
2026-05-28 10:17:28 +0200

    Update RecipeDTO.java
    DTO

--- src/main/java/DTO/RecipeDTO.java
+++ src/main/java/DTO/RecipeDTO.java
@@ -1,6 +1,7 @@
 package DTO;
 
 import Controller.RecipeController;
+import Model.Cuisine;
 import Model.Ingredient;
 import Model.Recipe;
 
@@ -13,20 +14,27 @@ import java.util.List;
  * @author Lena
  */
 public class RecipeDTO {
-    private RecipeController controller;
     private String name;
     private String instructions;
     private String imageUrl;
     private List<Ingredient> ingredients;
+    private Cuisine cuisine;
+    private double matchPercentage;
 
     /**
      * Constructs a RecipeDTO with minimal display information.
      * @param name the name of the recipe
      * @param imageUrl the URL of the recipe image
+     * @param instructions cooking instructions
+     * @param ingredients list of ingredients with measures
+     * @param matchPercentage how well this recipe matched the users fridge
      */
-    public RecipeDTO(String name, String imageUrl ){
+    public RecipeDTO(String name, String imageUrl, String instructions, List<Ingredient> ingredients, Cuisine cuisine, double matchPercentage ){
         this.name = name;
         this.imageUrl = imageUrl;
+        this.instructions = instructions;
+        this.cuisine = cuisine;
+        this.matchPercentage = matchPercentage;
     }
 
     /**
@@ -43,5 +51,17 @@ public class RecipeDTO {
      */
     public String getImageUrl() {
         return imageUrl;
+    }
+
+    public List<Ingredient> getIngredients() {return ingredients;}
+
+    public Cuisine getCuisine() { return cuisine;}
+
+    public String getInstructions() {return instructions;}
+
+    public double getMatchPercentage(){ return matchPercentage;}
+
+    public String getMatchPercentageFormatted(){
+        return (int) Math.round(matchPercentage * 100) + "%";
     }
 }
